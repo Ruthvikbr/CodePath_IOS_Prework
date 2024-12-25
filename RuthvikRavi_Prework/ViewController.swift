0354078c733ecd43e8c4ffd3f537b3d9cd4fc2a6
RuthvikRavi <ravi.ru@northeastern.edu>
2024-12-24 23:40:54 -0500

    Initial Commit

---
+++ RuthvikRavi_Prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RuthvikRavi_Prework
+//
+//  Created by Ruthvik Ravi on 12/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
